5e2683a28987f1967a015be3c907008452a2adaa
Ivan Gargov <ivan.gargov@telelink.com>
2017-12-14 22:03:54 +0200

    BasicMenuSkeleton

--- DeliverIT.Program/DeliverITProgramMain.cs
+++ DeliverIT.Program/DeliverITProgramMain.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DeliverIT.Core.Engine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,9 @@ namespace DeliverIT.Program
 {
     public class DeliverITProgramMain
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            InteractiveMenu.Interactive.Start();
         }
     }
 }
