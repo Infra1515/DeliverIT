440d698613577b3e90b2437c1c5e8022ce1eef00
Irina Hristova <hristova119@gmail.com>
2018-02-08 01:37:44 +0200

    Added DataStore class for storing users and orders

---
+++ DeliverIT.Core/Contracts/IDataStore.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using DeliverIT.Contracts;
+
+namespace DeliverIT.Core.Contracts
+{
+    public interface IDataStore
+    {
+        ICollection<IUser> Users { get; }
+
+        ICollection<IOrder> Orders { get; }
+    }
+}
---
+++ DeliverIT.Core/Engine/DataStorage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using DeliverIT.Contracts;
+using DeliverIT.Core.Contracts;
+
+namespace DeliverIT.Core.Engine
+{
+    public class DataStorage : IDataStore
+    {
+        public ICollection<IUser> Users { get; }
+
+        public ICollection<IOrder> Orders { get; }
+    }
+}
