b0506e1566197e8c162f3aa729f1e77a1af52802
Irina Hristova <hristova119@gmail.com>
2018-02-08 01:12:59 +0200

    Implemented ShowAllOrdersCommand and ShowAllClientsCommand

--- DeliverIT.Core/Commands/ShowAllClientsCommand.cs
+++ DeliverIT.Core/Commands/ShowAllClientsCommand.cs
@@ -1,12 +1,38 @@
-﻿using DeliverIT.Core.Contracts;
+﻿using System.Collections.Generic;
+using System.Text;
+using DeliverIT.Contracts;
+using DeliverIT.Core.Contracts;
 
 namespace DeliverIT.Core.Commands
 {
     public class ShowAllClientsCommand : ICommand
     {
+        private readonly ICollection<IUser> users;
+
+        public ShowAllClientsCommand(ICollection<IUser> users)
+        {
+            this.users = users;
+        }
+
         public void Execute()
         {
-            throw new System.NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+            if (this.users.Count == 0)
+            {
+                sb.AppendLine("No clients registered!");
+            }
+            else
+            {
+                foreach (var client in this.users)
+                {
+                    sb.AppendLine(client.ToString());
+                    sb.AppendLine("-----------------------");
+                }
+            }
+
+            // todo fix returns 
+
+            //return sb.ToString();
         }
     }
 }
--- DeliverIT.Core/Commands/ShowAllOrdersCommand.cs
+++ DeliverIT.Core/Commands/ShowAllOrdersCommand.cs
@@ -1,12 +1,37 @@
-﻿using DeliverIT.Core.Contracts;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DeliverIT.Contracts;
+using DeliverIT.Core.Contracts;
 
 namespace DeliverIT.Core.Commands
 {
     public class ShowAllOrdersCommand : ICommand
     {
+        private readonly ICollection<IOrder> orders;
+
+        public ShowAllOrdersCommand(ICollection<IOrder> orders)
+        {
+            this.orders = orders;
+        }
+
         public void Execute()
         {
-            throw new System.NotImplementedException();
+            var sb = new StringBuilder();
+            if (this.orders.Any())
+            {
+                foreach (var order in orders)
+                {
+                    sb.AppendLine(order.ToString());
+                }
+            }
+            else
+            {
+                sb.AppendLine("No orders placed yet!");
+            }
+
+            // todo fix returns
+            //return sb.ToString();
         }
     }
 }
