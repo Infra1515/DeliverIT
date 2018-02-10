18a3f99a2e9ea6c25ec366ea34eac3ad89ac7183
Irina Hristova <hristova119@gmail.com>
2018-02-10 13:40:59 +0200

    Implemented ShowAllLocationsCommand with reflection

--- DeliverIT.Core/Commands/ShowAllLocationsCommand.cs
+++ DeliverIT.Core/Commands/ShowAllLocationsCommand.cs
@@ -1,7 +1,12 @@
-﻿using System.Text;
-using DeliverIT.Common.Enums;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using DeliverIT.Contracts;
 using DeliverIT.Core.Contracts;
 using DeliverIT.Core.IOUtilities.Contracts;
+using DeliverIT.Models.Countries;
 
 namespace DeliverIT.Core.Commands
 {
@@ -18,11 +23,22 @@ namespace DeliverIT.Core.Commands
         {
             StringBuilder strBuilder = new StringBuilder();
             strBuilder.AppendLine("--- Delivery locations ---");
-            strBuilder.AppendLine(" -- " + CountryType.Bulgaria);
-            strBuilder.AppendLine(" -- " + CountryType.Russia);
-            strBuilder.AppendLine(" -- " + CountryType.Serbia);
-            
+            //strBuilder.AppendLine(" -- " + CountryType.Bulgaria);
+            //strBuilder.AppendLine(" -- " + CountryType.Russia);
+            //strBuilder.AppendLine(" -- " + CountryType.Serbia);
+
+            var countries = FindDerivedTypes(Assembly.GetAssembly(typeof(ICountry)), typeof(Country));
+            foreach (var country in countries)
+            {
+                strBuilder.AppendLine($" -- {country.Name}");
+            }
+
             this.writer.WriteLine(strBuilder.ToString());
+        }
+
+        public IEnumerable<Type> FindDerivedTypes(Assembly assembly, Type baseType)
+        {
+            return assembly.GetTypes().Where(t => t.IsSubclassOf(baseType));
         }
     }
 }
