6ffb1627f8ce7e49ee5e4b0eb2ec6171d017c34a
Irina Hristova <hristova119@gmail.com>
2018-02-13 13:43:39 +0200

    Changed tests for address

---
+++ Tests/Models/AddressTests/Address_Should.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using DeliverIT.Data.Models;
+using DeliverIT.Data.Models.Countries;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.Models.AddressTests
+{
+    [TestClass]
+    public class Address_Should
+    {
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenCityIsNull()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => new Address(new Bulgaria(), "Bulgaria", "81", null));
+        }
+
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenCountryIsNull()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => new Address(null, "Bulgaria", "81", "Lovech"));
+        }
+
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenStreetNameIsNull()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => new Address(new Bulgaria(), null, "81", "Lovech"));
+        }
+
+        [TestMethod]
+        public void ThrowArgumentNullException_WhenStreetNumberIsNull()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => new Address(new Bulgaria(), "Bulgaria", null, "Lovech"));
+        }
+    }
+}
